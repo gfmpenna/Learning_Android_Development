4494220231b37cd6737efab466b1c06cad0da887
Guilherme Mendes <92112616+gfmpenna@users.noreply.github.com>
2023-07-29 08:35:04 -0300

    29-07-23

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            var nome = binding.editTextText.text.toString()
+            binding.textResultado.text = nome.toString()
+
+        }
+        binding.BTNCalc.setOnClickListener {
+            var calc = binding.EDTCalc1.text.toString().toDouble()
+            var calc2 = binding.EDTCalc2.text.toString().toDouble()
+            var result = calc * calc2
+            binding.textResultado.text = result.toDouble().toString()
+
+
+        }
+
     }
+
+
 }
